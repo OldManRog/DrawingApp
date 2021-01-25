46daf183efbd9373051eecfba42fb9e9756e4ec7
Rogerr Oliva <Roliva75@yahoo.com>
2021-01-25 16:29:54 -0600

    added body stlying and change canvas to a square

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const canvas = document.getElementById('canvas');
 
 const ctx = canvas.getContext('2d');
 
+let size = 30;
+
 canvas.addEventListener('mousedown', () => {
 
 });
@@ -9,4 +11,8 @@ canvas.addEventListener('mousedown', () => {
 function drawCircle(x,y) {
     ctx.beginPath();
     ctx.arc(x, y, size, 0, Math.PI * 2)
+    ctx.stroke()
 }
+
+
+drawCircle(200,200);
