5e9c67d9e5c7c1d04acd230ef5134acb52dd45a2
Rogerr Oliva <Roliva75@yahoo.com>
2021-01-25 17:07:49 -0600

    added width and height stlying to inline html and also addded function to draw

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const canvas = document.getElementById('canvas');
 
 const ctx = canvas.getContext('2d');
 
-let size = 30;
+let size = 50;
+let x = 250;
+let y = 250;
 
 canvas.addEventListener('mousedown', () => {
 
@@ -10,9 +12,16 @@ canvas.addEventListener('mousedown', () => {
 
 function drawCircle(x,y) {
     ctx.beginPath();
-    ctx.arc(x, y, size, 0, Math.PI * 2)
-    ctx.stroke()
+    ctx.arc(x, y, size, 0, Math.PI * 2);
+    ctx.fill();
 }
 
 
-drawCircle(200,200);
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawCircle(x,y);
+    requestAnimationFrame(draw);
+
+}
+
+draw();
