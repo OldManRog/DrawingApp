b15c5b0da70b2dba911c62ed81ce34c5930d7494
Rogerr Oliva <Roliva75@yahoo.com>
2021-01-25 21:32:15 -0600

    added drawLine, clearCanvas function and eventListener.
    - added colorEL event listener. then color is assign to the stroke/fill style on the circle and line
    
    - added new function drawLine which will connect the dots making the drawing smoother. The lineWidth is multiplied by 2 to make it look better
    
    - Added ClearCanvas function that is called when the clearCanvas event listener is clicked.
    
    -

--- main.js
+++ main.js
@@ -2,34 +2,61 @@ const canvas = document.getElementById('canvas');
 const increaseBtn = document.getElementById('increase');
 const decreaseBtn = document.getElementById('decrease');
 const sizeEl = document.querySelector('span');
+const colorEl = document.getElementById('color');
+const clearEl = document.querySelector('.clear');
+console.log(clearEl)
 
 const ctx = canvas.getContext('2d');
 
 let size = 20;
 let mouseClicked = false;
+let color = 'black';
+let x = undefined;
+let y = undefined;
 
 canvas.addEventListener('mousedown', (e) => {
     mouseClicked = true;
+
+    x = e.offsetX;
+    y = e.offsetY;
 })
 
 canvas.addEventListener('mouseup', (e) => {
     mouseClicked = false;
+
+     x = undefined;
+     y = undefined;
 })
 
 
 canvas.addEventListener('mousemove', (e) => {
     if(mouseClicked){
-    const x = e.offsetX
-    const y = e.offsetY
+    const x2 = e.offsetX
+    const y2 = e.offsetY
 
-    drawCircle(x,y)
+    drawCircle(x2, y2)
+    drawLine(x, y, x2, y2);
+    
+    x = x2;
+    y = y2;
+
     }
 });
 
 function drawCircle(x,y) {
     ctx.beginPath();
     ctx.arc(x, y, size, 0, Math.PI * 2);
+    ctx.fillStyle = color;
     ctx.fill();
+}
+
+function drawLine(x1, y1, x2, y2) {
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.strokeStyle = color;
+    ctx.lineWidth = size * 2;
+    ctx.stroke();
 }
 
 increaseBtn.addEventListener('click',(e) => {
@@ -53,9 +80,23 @@ decreaseBtn.addEventListener('click',(e) => {
     updateSizeOnSpan()
 })
 
+colorEl.addEventListener('change', (e) => {
+    color = e.target.value;
+})
+
+clearEl.addEventListener('click', () => {
+    clearCanvas()
+})
+
+function clearCanvas() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
 function updateSizeOnSpan() {
     sizeEl.innerHTML = ` ${size}`
 }
+
+
 
 /* function draw() {
     //ctx.clearRect(0, 0, canvas.width, canvas.height);
