912dfc7c4133c28941259fc5df6e93950b53ad0e
Rogerr Oliva <Roliva75@yahoo.com>
2021-01-25 18:38:47 -0600

    Only draw when mouse is mousedown
    - added 2 new event listeners.
       - if you mousedown, then Mouseclicked is true
       - if mouse is up, mouseclicked in false.
    
    - added a new condition to mousemove, in which only call draw function if move is mousedown
    
    - I also reduced the size of the circle

--- main.js
+++ main.js
@@ -2,14 +2,25 @@ const canvas = document.getElementById('canvas');
 
 const ctx = canvas.getContext('2d');
 
-let size = 50;
+let size = 20;
+let mouseClicked = false;
+
+canvas.addEventListener('mousedown', (e) => {
+    mouseClicked = true;
+})
+
+canvas.addEventListener('mouseup', (e) => {
+    mouseClicked = false;
+})
 
 
 canvas.addEventListener('mousemove', (e) => {
+    if(mouseClicked){
     const x = e.offsetX
     const y = e.offsetY
 
     drawCircle(x,y)
+    }
 });
 
 function drawCircle(x,y) {
